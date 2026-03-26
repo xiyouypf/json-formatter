a28a46f00702fd46648eadf0b63a90170af495d5
yangpengfei11 <yangpengfei11@meituan.com>
2026-03-27 01:18:07 +0800

    feat: replace Monetag with new ad provider

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
- "domain": "3nbf4.com",
- "zoneId": 10788433
+ "domain": "5gvci.com",
+ "zoneId": 10790278
 }
 self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
